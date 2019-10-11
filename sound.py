7ce45791f8f7ef2485aabccff39a509d23d6bfc1
Ahmed Khalil <ahmed.bn.khalil@gmail.com>
2019-10-11 21:45:52 +0200

    experimenting with various feature extraction functions

--- sound.py
+++ sound.py
@@ -21,6 +21,9 @@ from scipy.fftpack import fft
 from python_speech_features import mfcc
 from python_speech_features import delta
 
+x  = np.linspace(0,400-1,400,dtype=np.int64)
+hw = 0.54 - 0.46 * np.cos(2*np.pi*(x)/(400-1))
+
 def read_wav(filename):
     """ read sound files of type wave """
     wavfile      = wave.open(filename, 'rb')
@@ -38,8 +41,8 @@ def read_wav(filename):
 
 # print(read_wav('experimental/speech.wav')[0:50])
 
-def get_mfcc(wav, samplerate):
-    """ get mfcc features """
+def get_freq_features(wav, samplerate):
+    """ get freq features """
     if(samplerate != 16000):
         raise ValueError('Allowed only at 16kHz')
 
@@ -70,6 +73,67 @@ def get_mfcc(wav, samplerate):
     return inputdata
 
 
+
+def get_mfcc_features(wav, samplerate):
+    f_mfcc   = mfcc(wav[0], samplerate)
+    f_mfccd  = delta(f_mfcc, 2)
+    f_mfccdd = delta(f_mfccd, 2)
+    wav_feat = np.column_stack((f_mfcc, f_mfccd, f_mfccdd))
+    
+    return wav_feat
+
+
+def get_freq_features2(wav, samplerate):
+    """ get freq features """
+    if(samplerate != 16000):
+        raise ValueError('Allowed only at 16kHz')
+
+    timewindow = 25
+    windowlen  = samplerate/1000*timewindow
+
+    wavarray = np.array(wav)
+    wavlen   = wavarray.shape[1]
+
+    rangeall  = int(len(wav[0])/samplerate*1000 - timewindow) // 10
+    datainput = np.zeros((1,400), dtype=np.float)
+    dataline  = np.zeros((1,400), dtype=np.float)
+    for i in range(0, rangeall):
+        pstart       = i * 160
+        pend         = pstart + 400
+        dataline     = wavarray[0,pstart:pend]
+        datainput[i] = dataline[0:200]
+
+    return datainput
+
+# data, fs = read_wav('experimental/speech.wav')
+# print(get_freq_features2(data, 16000))
+
+def get_freq_features3(wav, samplerate):
+    """ get freq features """
+    if(samplerate != 16000):
+        raise ValueError('Allowed only at 16kHz')
+
+    timewindow = 25
+    windowlen  = samplerate/1000*timewindow
+
+    wavarray = np.array(wav)
+    wavlen   = wavarray.shape[1]
+
+    rangeall  = int(len(wav[0])/samplerate*1000 - timewindow) // 10
+    datainput = np.zeros((rangeall,200), dtype=np.float)
+    dataline  = np.zeros((1,400), dtype=np.float)
+    for i in range(0, rangeall):
+        pstart       = i * 160
+        pend         = pstart + 400
+
+        dataline     = wavarray[0,pstart:pend]
+        dataline     = dataline * hw
+        dataline     = np.abs(fft(dataline)) / wavlen
+        datainput[i] = dataline[0:200]
+
+    datainput = np.log(datainput+1)
+
+    return datainput
 
 
 
